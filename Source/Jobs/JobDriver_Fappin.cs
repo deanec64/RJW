aa9cd0712e470fc26bf50fc5f60d76a59c8c9eb6
SuperHotSausage <32860054+SuperHotSausage@users.noreply.github.com>
2017-11-01 14:07:18 +0700

    Standing fapping bug test fix. (5)

--- Source/Jobs/JobDriver_Fappin.cs
+++ Source/Jobs/JobDriver_Fappin.cs
@@ -6,15 +6,15 @@ using Verse;
 using Verse.AI;
 using RimWorld;
 
-namespace rjw
-{
-	public class JobDriver_Fappin : JobDriver
-	{
+namespace rjw {
+	public class JobDriver_Fappin : JobDriver {
 
 		private const int ticks_between_hearts = 100;
-
+		
 		private int ticks_left;
-
+		
+		private TargetIndex ibed = TargetIndex.A;
+		
 		private static readonly SimpleCurve fap_interval_from_age = new SimpleCurve {
 			new CurvePoint (16f, 0.75f),
 			new CurvePoint (22f, 0.75f),
@@ -22,70 +22,48 @@ namespace rjw
 			new CurvePoint (50f, 3.00f),
 			new CurvePoint (75f, 5.00f)
 		};
-
-		private const TargetIndex BedOrRestSpotIndex = TargetIndex.A;
-
+		
 		private Building_Bed Bed
 		{
-			get
-			{
-				return (Building_Bed)base.CurJob.GetTarget(TargetIndex.A).Thing;
+			get {
+				return (Building_Bed)((Thing)base.CurJob.GetTarget(ibed));
 			}
 		}
-
-		protected override IEnumerable<Toil> MakeNewToils()
+		
+		protected override IEnumerable<Toil> MakeNewToils ()
 		{
-			ticks_left = (int)(2000.0f * Rand.Range(0.20f, 0.70f));
+			ticks_left = (int)(2500.0f * Rand.Range (0.20f, 0.70f));
+			
+			this.FailOnDespawnedOrNull (ibed);
+			this.KeepLyingDown (ibed);
+			yield return Toils_Bed.ClaimBedIfNonMedical (ibed, TargetIndex.None);
+			yield return Toils_Bed.GotoBed (ibed);
 
-			this.FailOnDespawnedOrNull(TargetIndex.A);
-			this.KeepLyingDown(TargetIndex.A);
-
-			//bool hasBed = this.pawn.CurJob.GetTarget(TargetIndex.A).HasThing;
-
-			//yield return Toils_Reserve.Reserve(TargetIndex.A, this.Bed.SleepingSlotsCount, 0, null);
-			//yield return Toils_Bed.ClaimBedIfNonMedical(TargetIndex.A, TargetIndex.None);
-			//yield return Toils_Bed.GotoBed(TargetIndex.A);
-
-			yield return Toils_Goto.GotoCell(TargetIndex.A, PathEndMode.OnCell);
-
-			Toil do_fappin = Toils_LayDown.LayDown(TargetIndex.A, false, false, false, false);
-
-			do_fappin.initAction = delegate
-			{
-				Log.Message("[RJW]JobDriver_Fappin::MakeNewToils - do_fappin.initAction is called");
-			};
-
-			do_fappin.AddPreTickAction(delegate
-			{
+			Toil do_fappin = Toils_LayDown.LayDown (ibed, true, false, false, false);
+			do_fappin.AddPreTickAction (delegate {
 				--this.ticks_left;
-				if (this.ticks_left <= 0)
-					this.ReadyForNextToil();
-				else if (pawn.IsHashIntervalTick(ticks_between_hearts))
-					MoteMaker.ThrowMetaIcon(pawn.Position, pawn.Map, ThingDefOf.Mote_Heart);
+			    if (this.ticks_left <= 0)
+			    	this.ReadyForNextToil ();
+			    else if (pawn.IsHashIntervalTick (ticks_between_hearts))
+			    	MoteMaker.ThrowMetaIcon (pawn.Position, pawn.Map, ThingDefOf.Mote_Heart);
 			});
-
-			do_fappin.AddFinishAction(delegate
-			{
-				//Thought_Memory newThought = (Thought_Memory)ThoughtMaker.MakeThought(ThoughtDefOf.Fapped);
-				pawn.mindState.canLovinTick = Find.TickManager.TicksGame + generate_min_ticks_to_next_fappin(pawn);
-				xxx.satisfy(pawn, null);
+			do_fappin.AddFinishAction (delegate {
+			    pawn.mindState.canLovinTick = Find.TickManager.TicksGame + generate_min_ticks_to_next_fappin (pawn);
+				xxx.satisfy (pawn, null);
 			});
-
 			do_fappin.socialMode = RandomSocialMode.Off;
 			yield return do_fappin;
 		}
-
-		private int generate_min_ticks_to_next_fappin(Pawn p)
+		
+		private int generate_min_ticks_to_next_fappin (Pawn p)
 		{
-			if (!DebugSettings.alwaysDoLovin)
-			{
-				var interval = fap_interval_from_age.Evaluate(pawn.ageTracker.AgeBiologicalYearsFloat);
-				var rinterval = Math.Max(0.5f, Rand.Gaussian(interval, 0.3f));
-				return (int)((xxx.is_nympho(p) ? 0.5f : 1.0f) * rinterval * 2500.0f);
-			}
-			else
+			if (! DebugSettings.alwaysDoLovin) {
+				var interval = fap_interval_from_age.Evaluate (pawn.ageTracker.AgeBiologicalYearsFloat);
+				var rinterval = Math.Max (0.5f, Rand.Gaussian (interval, 0.3f));
+				return (int)((xxx.is_nympho (p) ? 0.5f : 1.0f) * rinterval * 2500.0f);
+			} else
 				return 50;
 		}
-
+		
 	}
 }
