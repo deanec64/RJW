5eb03f1b64bddf90bfca949d4232219709c1c3c8
SuperHotSausage <32860054+SuperHotSausage@users.noreply.github.com>
2017-11-01 21:41:53 +0700

    CP button test fix.

--- Source/Common/Designator_ComfortPrisoner.cs
+++ Source/Common/Designator_ComfortPrisoner.cs
@@ -15,7 +15,7 @@ namespace rjw {
 			icon = comfort_prisoner_invisible_tex.gizmo;
 
 			// TODO: Can this be null?
-			hotKey = KeyBindingDefOf.Misc12;
+			//hotKey = KeyBindingDefOf.Misc12;
 			
 			// These don't matter but set them just in case
 			soundDragSustain = SoundDefOf.DesignateDragStandard;
@@ -31,7 +31,8 @@ namespace rjw {
 		public override AcceptanceReport CanDesignateThing (Thing t)
 		{
             var p = t as Pawn;
-            return (p != null) && (p.IsPrisonerOfColony || p.IsColonist || p.Faction == Faction.OfPlayer) && xxx.can_get_raped(p) && (!comfort_prisoners.is_designated(p));
+			//return (p != null) && (p.IsPrisonerOfColony || p.IsColonist || p.Faction == Faction.OfPlayer) && xxx.can_get_raped(p) && (!comfort_prisoners.is_designated(p));
+			return (p != null) && (p.IsPrisonerOfColony) && (!comfort_prisoners.is_designated(p)); //comfor prisoner button will only appear on prisoners
 		}
 		
 		public override void DesignateThing (Thing t)
