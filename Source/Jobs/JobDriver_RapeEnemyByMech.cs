cfa209e9cb0f90944375b9427e92aa1c64027594
hogehoge1942 <hogehoge_1942@yahoo.co.jp>
2017-11-01 18:33:37 +0900

    Merging ABF Complete

---
+++ Source/Jobs/JobDriver_RapeEnemyByMech.cs
@@ -0,0 +1,25 @@
+﻿
+using Verse;
+
+namespace rjw
+{
+	class JobDriver_RapeEnemyByMech : JobDriver_RapeEnemy
+	{
+		public override void aftersex(Pawn pawn, Pawn part, bool violent = false, bool isCoreLovin = false, bool isAnalSex = false)
+		{
+			base.aftersex(pawn, part, violent, isCoreLovin, isAnalSex);
+
+			/*if (pawn.raceprops.ismechanoid && xxx.is_human(part))
+            {
+                log.message("[abf]addmechaftersex::pretfix() - mech raped humans");
+                if (isanalsex)
+                {
+                    implanttoanal("microcomputer", part);
+                }
+                else {
+                    implanttogenital("microcomputer", part);
+                }
+            }*/
+		}
+	}
+}
---
+++ Source/Trigger/Trigger_SexSatisfy.cs
@@ -0,0 +1,46 @@
+﻿
+using Verse;
+using Verse.AI.Group;
+
+namespace rjw
+{
+
+	public class Trigger_SexSatisfy : TriggerFilter
+	{
+		private const int CheckInterval = 120;
+		private const int TickTimeout = 900;
+		private int currentTick = 0;
+		public float targetValue = 0.3f;
+
+		public Trigger_SexSatisfy(float t)
+		{
+			this.targetValue = t;
+			currentTick = 0;
+		}
+
+
+		public override bool AllowActivation(Lord lord, TriggerSignal signal)
+		{
+			currentTick++;
+			if (signal.type == TriggerSignalType.Tick && Find.TickManager.TicksGame % CheckInterval == 0)
+			{
+				float? avgValue = null;
+				foreach (var pawn in lord.ownedPawns)
+				{
+					/*foreach(Pawn p in lord.Map.mapPawns.PawnsInFaction(Faction.OfPlayer))
+                    {
+
+                    }*/
+					Need_Sex n = pawn.needs.TryGetNeed<Need_Sex>();
+					if (n != null && pawn.gender == Gender.Male && !pawn.Downed)
+					{
+						avgValue = (avgValue == null) ? n.CurLevel : (avgValue + n.CurLevel) / 2f;
+					}
+				}
+				Log.Message("[ABF]Trigger_SexSatisfy::ActivateOn Checked value :" + avgValue + "/" + targetValue);
+				return avgValue >= targetValue;
+			}
+			return currentTick >= TickTimeout;
+		}
+	}
+}
