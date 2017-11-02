10cff4ce83cb18188c26ee239c3065721cc72be4
hogehoge1942 <hogehoge_1942@yahoo.co.jp>
2017-11-02 13:37:44 +0900

    Not Code copy but use thinktree

--- Source/Jobs/JobGiver_DoFappin.cs
+++ Source/Jobs/JobGiver_DoFappin.cs
@@ -12,32 +12,6 @@ namespace rjw {
 		protected override Job TryGiveJob (Pawn p)
         {
             Log.Message("[RJW] JobGiver_DoFappin::TryGiveJob( " + p.NameStringShort + " ) called");
-
-			//Copied from JobGiver_DoLovin Try lovin before fappin then fappin
-			if (Find.TickManager.TicksGame < p.mindState.canLovinTick)
-			{
-				goto SkipLovin;
-			}
-			if (p.CurrentBed() == null || p.CurrentBed().Medical || !p.health.capacities.CanBeAwake)
-			{
-				goto SkipLovin;
-			}
-			Pawn partnerInMyBed = LovePartnerRelationUtility.GetPartnerInMyBed(p);
-			if (partnerInMyBed == null || !partnerInMyBed.health.capacities.CanBeAwake || Find.TickManager.TicksGame < partnerInMyBed.mindState.canLovinTick)
-			{
-				goto SkipLovin;
-			}
-			if (!p.CanReserve(partnerInMyBed, 1, -1, null, false) || !partnerInMyBed.CanReserve(p, 1, -1, null, false))
-			{
-				goto SkipLovin;
-			}
-			p.mindState.awokeVoluntarily = true;
-			partnerInMyBed.mindState.awokeVoluntarily = true;
-			return new Job(JobDefOf.Lovin, partnerInMyBed, p.CurrentBed());
-
-
-
-			SkipLovin:
 			if ((Find.TickManager.TicksGame >= p.mindState.canLovinTick) &&
 				(p.CurJob != null)) {
 
