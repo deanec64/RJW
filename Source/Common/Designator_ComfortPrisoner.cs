e0915851c936e457af54d2a05129c6ffea358db7
SuperHotSausage <superhotbeefsteak@gmail.com>
2017-11-03 09:42:02 +0700

    CP button test fix. (7)

--- Source/Common/Designator_ComfortPrisoner.cs
+++ Source/Common/Designator_ComfortPrisoner.cs
@@ -68,7 +68,7 @@ namespace rjw
 			defaultLabel = MTdef.label;
 			defaultDesc = MTdef.description;
 
-			icon = comfort_prisoner_tex.gizmo;
+			icon = ContentFinder<Texture2D>.Get("UI/Commands/UI/Commands/comfort_prisoner_true");
 
 			// TODO: Can this be null?
 			hotKey = KeyBindingDefOf.Misc12;
