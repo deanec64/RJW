b952d0bec46d4045a37de3326b7d48c65fc7f520
SuperHotSausage <superhotbeefsteak@gmail.com>
2017-11-05 11:23:42 +0700

    Nymp join bed test bugfix. (10)

--- Source/Jobs/JobDriver_NymphJoinInBed.cs
+++ Source/Jobs/JobDriver_NymphJoinInBed.cs
@@ -13,15 +13,11 @@ namespace rjw
 
 		private const int ticks_between_hearts = 100;
 
-		private const int ticks_between_thrusts = 150;
-
 		private int ticks_left;
 
 		private TargetIndex ipartner = TargetIndex.A;
 
 		private TargetIndex ibed = TargetIndex.B;
-
-		private bool isAnalSex = false;
 
 		protected Pawn Partner
 		{
@@ -41,55 +37,57 @@ namespace rjw
 
 		protected override IEnumerable<Toil> MakeNewToils()
 		{
-			Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() called");
+			//Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() called");
 			this.FailOnDespawnedOrNull(ipartner);
 			this.FailOnDespawnedOrNull(ibed);
 			this.FailOn(() => !Partner.health.capacities.CanBeAwake);
 			this.FailOn(() => !xxx.is_laying_down_alone(Partner));
 			yield return Toils_Reserve.Reserve(ipartner, comfort_prisoners.max_rapists_per_prisoner, 0);
 			yield return Toils_Goto.GotoThing(ipartner, PathEndMode.OnCell);
-			bool pawnHasPenis = Genital_Helper.has_penis(pawn);
-
-			Toil do_lovin = new Toil();
+			yield return new Toil
+			{
+				initAction = delegate {
+					//Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() - setting initAction");
+					ticks_left = (int)(2500.0f * Rand.Range(0.30f, 1.30f));
+					var gettin_loved = new Job(xxx.gettin_loved, pawn, Bed);
+					Partner.jobs.StartJob(gettin_loved, JobCondition.InterruptForced, null, false, true, null);
+				},
+				defaultCompleteMode = ToilCompleteMode.Instant
+			};
+			var do_lovin = new Toil();
 			do_lovin.defaultCompleteMode = ToilCompleteMode.Never;
 			do_lovin.FailOn(() => (Partner.CurJob == null) || (Partner.CurJob.def != xxx.gettin_loved));
-
-			do_lovin.initAction = delegate
-			{
-				Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() - do_lovin.initAction is called");
-				ticks_left = (int)(2000.0f * Rand.Range(0.30f, 1.30f));
-				if (!pawnHasPenis)
-					Partner.Drawer.rotator.Face(pawn.DrawPos);
-
-				var gettin_loved = new Job(xxx.gettin_loved, pawn, Bed);
-				Partner.jobs.StartJob(gettin_loved, JobCondition.InterruptForced, null, false, true, null);
-			};
-			do_lovin.AddPreTickAction(delegate
-			{
+			do_lovin.AddPreTickAction(delegate {
 				--ticks_left;
-				if (pawn.IsHashIntervalTick(ticks_between_hearts))
-					MoteMaker.ThrowMetaIcon(pawn.Position, pawn.Map, ThingDefOf.Mote_Heart);
-				if (pawn.IsHashIntervalTick(ticks_between_thrusts))   // Should test whether this will fail the job
-					xxx.sexTick(pawn, Partner);
 				if (ticks_left <= 0)
 					ReadyForNextToil();
-			});
-			do_lovin.AddFinishAction(delegate
-			{
-				//// Trying to add some interactions and social logs
-				xxx.processAnalSex(pawn, Partner, ref isAnalSex, pawnHasPenis);
+				else if (pawn.IsHashIntervalTick(ticks_between_hearts))
+					MoteMaker.ThrowMetaIcon(pawn.Position, pawn.Map, ThingDefOf.Mote_Heart);
 			});
 			do_lovin.socialMode = RandomSocialMode.Off;
 			yield return do_lovin;
-
-			Toil afterLovin = new Toil
+			yield return new Toil
 			{
-				initAction = delegate
-				{
-					Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() - afterLovin.initAction is called");
+				initAction = delegate {
+					//Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() - setting pawn.got_some_lovin memory in second initAction");
+					var sex_mem = (Thought_Memory)ThoughtMaker.MakeThought(ThoughtDefOf.GotSomeLovin);
+					var pawn_memories = pawn.needs.mood.thoughts.memories as MemoryThoughtHandler;
+					if (pawn_memories != null)
+					{
+						pawn.needs.mood.thoughts.memories.TryGainMemory(sex_mem, Partner);
+					}
+
+					//Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() - setting Partner.got_some_lovin memory in second initAction");
+					var sex_mem2 = (Thought_Memory)ThoughtMaker.MakeThought(ThoughtDefOf.GotSomeLovin); // Is this neccessary?
+					if (Partner.needs != null && Partner.needs.mood != null && Partner.needs.mood.thoughts != null)
+					{
+						Partner.needs.mood.thoughts.memories.TryGainMemory(sex_mem2, pawn);
+					}
 
 					//Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() - calling aftersex in second initAction");
-					xxx.aftersex(pawn, Partner, false, false, isAnalSex);
+					xxx.aftersex(pawn, Partner);
+					//Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() - calling aftersex again in second initAction");
+					//xxx.aftersex (Partner, pawn);
 					//Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() - setting mindstate in second initAction");
 					pawn.mindState.canLovinTick = Find.TickManager.TicksGame + xxx.generate_min_ticks_to_next_lovin(pawn);
 					//Log.Message("JobDriver_NymphJoinInBed::MakeNewToils() - setting mindstate again in second initAction");
@@ -97,7 +95,6 @@ namespace rjw
 				},
 				defaultCompleteMode = ToilCompleteMode.Instant
 			};
-			yield return afterLovin;
 		}
 
 	}
