7aca6b60fd4fa918079f29d327653d11bd1929c7
hogehoge1942 <hogehoge_1942@yahoo.co.jp>
2017-11-05 20:15:35 +0900

    Adding MechImplantsFuntion

--- Source/Hediffs/HeDiff_MicroComputer.cs
+++ Source/Hediffs/HeDiff_MicroComputer.cs
@@ -11,21 +11,41 @@ namespace rjw
 {
 	class Hediff_MicroComputer : Hediff_MechImplants
 	{
-		protected int nextEventTick = 60000;
+		//protected int nextEventTick = 60000;
+		protected int nextEventTick = 2000;
 		public override void ExposeData()
 		{
 			Scribe_Values.Look<int>(ref this.nextEventTick, "nextEventTick", 60000, false);
 		}
 		public override void Tick()
 		{
-			base.Tick();
 			if (this.pawn.IsHashIntervalTick(nextEventTick))
 			{
 				Log.Warning("Something happen");
-				pawn.health.AddHediff(new Hediff_Orgasm());
-				nextEventTick = Rand.Range(30000, 90000);
+				pawn.health.AddHediff(DefDatabase<HediffDef>.GetNamed(randomEffect));
+				nextEventTick = Rand.Range(mcDef.minEventInterval, mcDef.maxEventInterval);
 			}
+			base.Tick();
 		}
+		protected HediffDef_MicroComputer mcDef
+		{
+			get { return ((HediffDef_MicroComputer)def); }
+		}
+		protected List<string> randomEffects
+		{
+			get{ return ((HediffDef_MicroComputer)def).randomHediffDefs; }
+		}
+		protected string randomEffect
+		{
+			get { return randomEffects[Rand.Range(0, randomEffects.Count)]; }
+		}
+	}
 
+	[StaticConstructorOnStartup]
+	class HediffDef_MicroComputer : HediffDef_EnemyImplants
+	{
+		public List<string> randomHediffDefs = new List<string>();
+		public int minEventInterval = 30000;
+		public int maxEventInterval = 90000;
 	}
 }
---
+++ Source/Hediffs/Hediff_MCEvents.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+using Verse;
+using RimWorld;
+using RimWorld.Planet;
+using System.Collections.Generic;
+
+namespace rjw
+{
+	class Hediff_Orgasm : HediffWithComps
+	{ 
+		public override void PostAdd(DamageInfo? dinfo)
+		{
+			Messages.Message("FeltOrgasm".Translate(new object[] { this.pawn.LabelIndefinite() }).CapitalizeFirst(), MessageSound.Standard);
+		}
+
+		public override bool TryMergeWith(Hediff other)
+		{
+			if (other == null || other.def != this.def || other.Part != this.Part)
+			{
+				return false;
+			}
+			this.Severity += other.Severity;
+			this.ageTicks = 0;
+			return true;
+		}
+	}
+	class Hediff_TransportCums : HediffWithComps
+	{
+		public override void PostAdd(DamageInfo? dinfo)
+		{
+			Messages.Message("CumsTransported".Translate(new object[] { this.pawn.LabelIndefinite() }).CapitalizeFirst(), MessageSound.Standard);
+
+			pawn.health.RemoveHediff(this);
+		}
+
+	}
+}
