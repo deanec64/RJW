bed7611719785edfadbad341d64f01fbb508d430
SuperHotSausage <superhotbeefsteak@gmail.com>
2017-11-05 18:27:14 +0700

    Temporary fix for anal rape.

--- Source/Jobs/JobGiver_ComfortPrisonerRape.cs
+++ Source/Jobs/JobGiver_ComfortPrisonerRape.cs
@@ -8,12 +8,13 @@ namespace rjw {
 
 		protected override Job TryGiveJob (Pawn p)
         {
-            Log.Message("[RJW] JobGiver_ComfortPrisonerRape::TryGiveJob( " + p.NameStringShort + " ) called0");
+			bool private wildmode = ModSettings.WildMode;
+			Log.Message("[RJW] JobGiver_ComfortPrisonerRape::TryGiveJob( " + p.NameStringShort + " ) called0");
             if ((Find.TickManager.TicksGame >= p.mindState.canLovinTick) && (p.CurJob == null)) {
 
                 Log.Message("[RJW] JobGiver_ComfortPrisonerRape::TryGiveJob( " + p.NameStringShort + " ) called1");
                 // don't allow pawns marked as comfort prisoners to rape others
-                if ((xxx.is_healthy(p) && xxx.can_rape(p,xxx.has_traits(p)&&xxx.is_nympho_or_rapist_or_zoophiliac(p)) && !comfort_prisoners.is_designated(p) ) || ModSettings.WildMode) {
+                if ((xxx.is_healthy(p) && xxx.can_rape(p,xxx.has_traits(p)&&xxx.is_nympho_or_rapist_or_zoophiliac(p)) && !comfort_prisoners.is_designated(p) ) || WildMode) {
 
                     //Log.Message("[RJW] JobGiver_ComfortPrisonerRape::TryGiveJob( " + p.NameStringShort + " ) called2");
                     Pawn prisoner = xxx.find_prisoner_to_rape(p, p.Map);
