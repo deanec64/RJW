77ee2f8d60a35cd763c7dac2bdc16b5121a01f5c
hogehoge1942 <hogehoge_1942@yahoo.co.jp>
2017-11-16 05:56:36 +0900

    MentalBreakRandomRape Exp

---
+++ Source/MentalState/MentalState_RandomRape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse.AI;
+using Verse;
+
+namespace rjw
+{
+	class MentalState_RandomRape : MentalState
+	{
+		public override bool ForceHostileTo(Thing t)
+		{
+			return false;
+		}
+
+		public override bool ForceHostileTo(Faction f)
+		{
+			return false;
+		}
+
+		public override RandomSocialMode SocialModeMax()
+		{
+			return RandomSocialMode.Off;
+		}
+	}
+}
