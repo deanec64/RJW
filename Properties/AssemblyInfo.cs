c0ea78a86845ff7b97dcb463c602a362a1f67041
SuperHotSausage <superhotbeefsteak@gmail.com>
2017-11-21 09:25:17 +0700

    Updated DLL.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.0.455")]
+[assembly: AssemblyVersion("1.6.0.456")]
 //[assembly: AssemblyFileVersion("1.6.0")]
 [assembly: AssemblyInformationalVersion("1.6.0")]
